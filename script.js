8e0785deb58778a0f2b63febbb09a8eb35d5ce89
Ridwan Suhel <65410320+Ridwan-Suhel@users.noreply.github.com>
2021-08-18 22:59:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let bulbImg = document.querySelector("#bulbImg");
+// let bulbImg = document.querySelector("#bulbImg");
+
+
+function onImgfunction(){
+    bulbImg.src = "images/pic_bulbon.gif";
+    // return btnOn;
+}
+
+function offImgfunction(){
+    bulbImg.src = "images/pic_bulboff.gif";
+    // return btnOn;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
